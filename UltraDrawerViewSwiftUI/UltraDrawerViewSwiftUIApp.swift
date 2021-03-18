db6d24c9551726bb31a141be22ce3bb077e95a24
Adrien <adrnsmn@gmail.com>
2021-03-18 21:23:40 +0100

    Initial Commit

---
+++ UltraDrawerViewSwiftUI/UltraDrawerViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltraDrawerViewSwiftUIApp.swift
+//  UltraDrawerViewSwiftUI
+//
+//  Created by Admin on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct UltraDrawerViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
